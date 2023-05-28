2e166e51b5e384000272e2da27c1fb1b4fffba04
Matt 1968 <pascal.amann.perso@gmail.com>
2023-05-28 18:14:20 +0200

    Ajout de la validité des dates du tournoi

--- controleurs/main_controller.py
+++ controleurs/main_controller.py
@@ -1,13 +1,12 @@
 from controleurs.base_controller import BaseController
+from controleurs.base_controller import BaseController
 from vues.main_view import MainView
+from controleurs.controleur_joueur import AddPlayerController
 from controleurs.player_list_controller import ShowPlayerListController
 from controleurs.nouveau_tournoi_controller import AddNewTournament
-from controleurs.controleur_joueur import AddPlayerController
-from vues.ajout_tournoi import CreationTournoiView
 
 
 class MainController(BaseController):
-    
     def execute(self):
         while True:
             view = MainView()
@@ -15,16 +14,16 @@ class MainController(BaseController):
             controller = None
             if choix == 1:
                 controller = AddNewTournament(self.database)
-            elif choix == 2:
-                controller = ShowPlayerListController(self.database)
-            elif choix == 3:
+            if choix == 2:
+                pass
+            if choix == 3:
                 pass
             elif choix == 4:
                 controller = AddPlayerController(self.database)
             if choix == 5:
                 controller = ShowPlayerListController(self.database)
             if choix == 6:
-                controller = AddMatches(self.database)
+                pass
             if choix == 7:
                 pass
             elif choix == 0:
--- controleurs/nouveau_tournoi_controller.py
+++ controleurs/nouveau_tournoi_controller.py
@@ -1,14 +1,14 @@
 from controleurs.base_controller import BaseController
 from modeles.tournoi import Tournoi
 from vues.affichage_tournoi_view import DisplayTournamentView
-from uuid import uuid4
+from uuid import UUID, uuid4
+from pprint import pprint
+from datetime import datetime
+import locale
 FILENAME = "data.json"
 
 
 class AddNewTournament (BaseController):
-
-    def __init__ (self):
-        pass
     
     def execute(self):
         view = DisplayTournamentView()
@@ -19,8 +19,9 @@ class AddNewTournament (BaseController):
         tournoi = Tournoi(uuid4(), data_tournoi["nom_tournoi"], data_tournoi["lieu_tournoi"], data_tournoi["date_debut_tournoi"], data_tournoi["date_fin_tournoi"], data_tournoii["type_tournoi"], data_tournoi["description"], data_tournoi["nombre_tours"])
         self.database.tournois.append(tournoi)
         self.database.save(FILENAME)
-     
-"""class CreationTournoiView:
+
+
+class CreationTournoiView:
     def __init__(self):
         pass
 
@@ -29,10 +30,26 @@ class AddNewTournament (BaseController):
         #id=uuid4()
         nom_tournoi = input("Nom du tournoi :\n")
         lieu_tournoi = input("Lieu du tournoi :\n")
-        date_debut_tournoi = input("Date de d�but du tournoi : ")
-        date_fin_tournoi = input("Date de fin du tournoi : (Appuyer sur Entr�e si le tournoi ne dure qu'un jour) ")
-        if not date_fin_tournoi:
-            date_fin_tournoi = date_debut_tournoi
+        locale.setlocale(locale.LC_ALL, "fr-FR.UTF8")
+        while True:
+            try:
+                date_debut = input("Date de dÃ©but du tournoi (au format JJ mois AAAA) : ")
+                date_debut_tournoi = datetime.strptime(date_debut, "%d %B %Y")
+                date_debut_tournoi = date_debut_tournoi.strftime("%d %b %Y")
+                break
+            except (ValueError, TypeError):
+                print("Format de date incorrect. Le format doit Ãªtre de type jj mois YYYY (ex : 20 juin 2023) avec un espace entre chaque.")
+
+        while True:
+            try:
+                date_fin = input("Date de fin du tournoi (au format JJ mois AAAA) : ")
+                date_fin_tournoi = datetime.strptime(date_fin, "%d %B %Y")
+                date_fin_tournoi = date_fin_tournoi.strftime("%d %b %Y")
+                print(f"Date de fin du tournoi : {date_fin_tournoi}")
+                break
+            except (ValueError, TypeError):
+                print("Format de date incorrect. Le format doit Ãªtre de type jj mois YYYY (ex : 21 juin 2023) avec un espace entre chaque.")
+
         while True:
             try:
                 type_tournoi = int(input("Type de tournoi : Rapide(1), Blitz(2), Bullet(3) (Rapide par d�faut) : \n"))
@@ -40,7 +57,8 @@ class AddNewTournament (BaseController):
                     break
             except ValueError:
                 print("La valeur doit �tre un nombre entier compris entre 1 et 3")
-        description = input("Description (Appuyer sur Entr�e pour ne pas saisir de description) \n")
+
+        description = input("Description facultative du directeur du tournoi\n")
         nombre_tours = 4
         libelle_type_tournoi = libelle_tournoi.get(str(type_tournoi))
         print(f"Type de tournoi : {libelle_type_tournoi}")
@@ -60,4 +78,4 @@ class AddNewTournament (BaseController):
         print(f"Type de tournoi : {TOURNAMENT_TYPE[type_tournoi]}")  # ({self.type_tournoi}) ")
         print(f"Description du directeur du tournoi : {description}")
 
-        return nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, type_tournoi, description, nombre_tours"""
+        return nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, type_tournoi, description, nombre_tours
