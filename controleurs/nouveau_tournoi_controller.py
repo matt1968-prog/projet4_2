3acb5172695f0fc3b94b9e76010927a2239d1fcb
Matt 1968 <pascal.amann.perso@gmail.com>
2023-05-29 15:31:12 +0200

    Suppression di lignes inutiles qui doivent figurer la vue du tournoi

--- controleurs/nouveau_tournoi_controller.py
+++ controleurs/nouveau_tournoi_controller.py
@@ -8,8 +8,11 @@ import locale
 FILENAME = "data.json"
 
 
-class AddNewTournament (BaseController):
+class AddNewTournament ():
     
+    def __init__(self, database):
+        self.database = database
+
     def execute(self):
         view = DisplayTournamentView()
         data_tournoi = view.display()
@@ -20,62 +23,3 @@ class AddNewTournament (BaseController):
         self.database.tournois.append(tournoi)
         self.database.save(FILENAME)
 
-
-class CreationTournoiView:
-    def __init__(self):
-        pass
-
-    def creer_tournoi(self):
-        libelle_tournoi = {'1': 'Rapide', '2': 'Blitz', '3': 'Bullet'}
-        #id=uuid4()
-        nom_tournoi = input("Nom du tournoi :\n")
-        lieu_tournoi = input("Lieu du tournoi :\n")
-        locale.setlocale(locale.LC_ALL, "fr-FR.UTF8")
-        while True:
-            try:
-                date_debut = input("Date de dÃ©but du tournoi (au format JJ mois AAAA) : ")
-                date_debut_tournoi = datetime.strptime(date_debut, "%d %B %Y")
-                date_debut_tournoi = date_debut_tournoi.strftime("%d %b %Y")
-                break
-            except (ValueError, TypeError):
-                print("Format de date incorrect. Le format doit Ãªtre de type jj mois YYYY (ex : 20 juin 2023) avec un espace entre chaque.")
-
-        while True:
-            try:
-                date_fin = input("Date de fin du tournoi (au format JJ mois AAAA) : ")
-                date_fin_tournoi = datetime.strptime(date_fin, "%d %B %Y")
-                date_fin_tournoi = date_fin_tournoi.strftime("%d %b %Y")
-                print(f"Date de fin du tournoi : {date_fin_tournoi}")
-                break
-            except (ValueError, TypeError):
-                print("Format de date incorrect. Le format doit Ãªtre de type jj mois YYYY (ex : 21 juin 2023) avec un espace entre chaque.")
-
-        while True:
-            try:
-                type_tournoi = int(input("Type de tournoi : Rapide(1), Blitz(2), Bullet(3) (Rapide par d�faut) : \n"))
-                if type_tournoi > 0 and type_tournoi < 4:
-                    break
-            except ValueError:
-                print("La valeur doit �tre un nombre entier compris entre 1 et 3")
-
-        description = input("Description facultative du directeur du tournoi\n")
-        nombre_tours = 4
-        libelle_type_tournoi = libelle_tournoi.get(str(type_tournoi))
-        print(f"Type de tournoi : {libelle_type_tournoi}")
-        # nouveau_tournoi = Tournoi(self.nom_tournoi, self.lieu_tournoi,
-        # self.date_debut_tournoi, self.date_fin_tournoi, self.type_tournoi, self.description, self.nombre_tours)
-
-    def afficher_tournoi(self):
-        TOURNAMENT_TYPE = {1: "Rapide", 2: "Blitz", 3: "Bullet"}
-        print()
-        print(f"R�capitulatif du nouveau tournoi {nom_tournoi} : ")
-        print()
-        print(f"Nom du tournoi : {nom_tournoi}")
-        print(f"Lieu : {lieu_tournoi}")
-        print(f"Date de d�but : {date_debut_tournoi}")
-        print(f"Date de fin : {date_fin_tournoi}")
-        type_tournoi = int(type_tournoi)
-        print(f"Type de tournoi : {TOURNAMENT_TYPE[type_tournoi]}")  # ({self.type_tournoi}) ")
-        print(f"Description du directeur du tournoi : {description}")
-
-        return nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, type_tournoi, description, nombre_tours
